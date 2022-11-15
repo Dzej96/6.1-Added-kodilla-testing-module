9e04ecbe2a491b3447cbc16cb44c3544794d7ac6
Jakub Jagielski <jakubaxix@interia.pl>
2022-11-15 15:52:08 +0100

    first commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int addresult1 = calculator.add(4,6);
+        if (addresult1 == 10) {
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        int subResult2 = calculator.substract(2,7);
+        if (subResult2 == 5) {
+            System.out.println("Substract test OK");
+        } else {
+                System.out.println("Error!");
+            }
+
+        }
     }
-}
+
